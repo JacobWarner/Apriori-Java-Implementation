4813c550bd583155672aa04d99fde465d4ab3bf7
Jacob Warner <jwarner@cyberlaunch.net>
2018-02-13 22:26:27 -0600

    Some other guys code lol

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,7 @@ import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
 import weka.core.converters.Loader;
 
+import javax.sound.midi.SysexMessage;
 import java.util.*;
 
 public class Main {
@@ -21,6 +22,7 @@ public class Main {
     private static List<int[]> dataEntriesWithEncodedStringIndices;
     private static HashMap<String,Integer> stringToIntegerEncoded = new HashMap<>();
     private static HashMap<Integer,String> integerToStringEncoded = new HashMap<>();
+    private static List <String> tupples = new ArrayList <> ();
     private static DataSource data = null;
     private static Instances instances = null;
 
@@ -32,10 +34,13 @@ public class Main {
             return;
         }
 
-        for (int i = 0; i < dataEntriesWithEncodedStringIndices.size(); i++) {
-            System.out.println(Arrays.toString(dataEntriesWithEncodedStringIndices.get(i)));
-        }
+        AprioriAlgorithm();
 
+//        for (int i = 0; i < dataEntriesWithEncodedStringIndices.size(); i++) {
+//            System.out.println(Arrays.toString(dataEntriesWithEncodedStringIndices.get(i)));
+//        }
+//
+        System.out.println();
         for (int j = 0; j < integerToStringEncoded.size(); j++) {
             System.out.println("Index=" + j + " ----> " + integerToStringEncoded.get(j));
         }
@@ -72,8 +77,8 @@ public class Main {
         }
     }
 
-    private static void AprioriAlgorithm() {
-        long startTime = System.currentTimeMillis();
+
+    private static void AprioriAlgorithm() throws Exception {
         // Generate of Candidates (C_k) is by joining L_{k-1} with itself
 
         // C_k: candidate itemset of size k
@@ -82,14 +87,38 @@ public class Main {
         // Create frequent items of size 1, L_1
         // k =2
         // while FrequentItemSet L_{k-1} not empty...
-            // Generate candidates from L_{k-1} with minSup
-            // for each transaction/instance (t) in database
-                // Generate subsets of t that are candidates (C_t)
-                // for each candidate c in C_t, c.count++
-            // L_k = {c in C_k : c.count >= minSup}
-            // k++;
+        // Generate candidates from L_{k-1} with minSup
+        // for each transaction/instance (t) in database
+        // Generate subsets of t that are candidates (C_t)
+        // for each candidate c in C_t, c.count++
+        // L_k = {c in C_k : c.count >= minSup}
+        // k++;
         // return L = U_k L_k
+
+        long startTime = System.currentTimeMillis();
+        createSizeOneItemSetsByEncodedIndexNumber();
+        int itemsetNumber=1; //the current itemset being looked at
+        int nbFrequentSets=0;
+
+        while (itemSetsByIndex.size()>0) {
+
+            calculateFrequentItemsets();
+
+            if(itemSetsByIndex.size()!=0)
+            {
+                nbFrequentSets+=itemSetsByIndex.size();
+                System.out.println("Found "+itemSetsByIndex.size()+" frequent itemsets of size " + itemsetNumber + " (with support "+(minSup*100)+"%)");
+                createNewItemsetsFromPreviousOnes();
+            }
+
+            itemsetNumber++;
+        }
         long endTime = System.currentTimeMillis();
+        System.out.println("Execution time is: "+((double)(endTime-startTime)/1000) + " seconds.");
+        System.out.println("Found "+nbFrequentSets+ " frequents sets for support "+(minSup*100)+"% (absolute "+Math.round(numInstances*minSup)+")");
+        System.out.println("Done");
+
+        Lister();
     }
 
     /**
@@ -99,7 +128,7 @@ public class Main {
     private static void createSizeOneItemSetsByEncodedIndexNumber() {
         itemSetsByIndex = new ArrayList<int[]>();
 
-        for (int i = 0; i < integerToStringEncoded.keySet().size(); i++) {
+        for (int i = 0; i < numAttributes*2; i++) {
             int[] candidate = {i};
             itemSetsByIndex.add(candidate);
         }
@@ -152,5 +181,206 @@ public class Main {
             }
             dataEntriesWithEncodedStringIndices.add(encodedDataEntry);
         }
+    }
+
+    private static void createNewItemsetsFromPreviousOnes()
+    {
+        // by construction, all existing itemsets have the same size
+        int currentSizeOfItemsets = itemSetsByIndex.get(0).length;
+
+        HashMap<String, int[]> tempCandidates = new HashMap<String, int[]>(); //temporary candidates
+
+        // compare each pair of itemsets of size n-1
+        for(int i=0; i<itemSetsByIndex.size(); i++)
+        {
+            for(int j=i+1; j<itemSetsByIndex.size(); j++)
+            {
+                int[] X = itemSetsByIndex.get(i);
+                int[] Y = itemSetsByIndex.get(j);
+
+                assert (X.length==Y.length);
+
+                //make a string of the first n-2 tokens of the strings
+                int [] newCand = new int[currentSizeOfItemsets+1];
+                for(int s=0; s<newCand.length-1; s++) {
+                    newCand[s] = X[s];
+                }
+
+                int ndifferent = 0;
+                // then we find the missing value
+                for(int s1=0; s1<Y.length; s1++)
+                {
+                    boolean found = false;
+                    // is Y[s1] in X?
+                    for(int s2=0; s2<X.length; s2++) {
+                        if (X[s2]==Y[s1]) {
+                            found = true;
+                            break;
+                        }
+                    }
+                    if (!found){ // Y[s1] is not in X
+                        ndifferent++;
+                        // we put the missing value at the end of newCand
+                        newCand[newCand.length -1] = Y[s1];
+                    }
+
+                }
+
+                // we have to find at least 1 different, otherwise it means that we have two times the same set in the existing candidates
+                assert(ndifferent>0);
+
+
+                if (ndifferent==1) {
+                    // HashMap does not have the correct "equals" for int[] :-(
+                    // I have to create the hash myself using a String :-(
+                    // I use Arrays.toString to reuse equals and hashcode of String
+                    Arrays.sort(newCand);
+                    tempCandidates.put(Arrays.toString(newCand),newCand);
+                }
+            }
+        }
+
+        //set the new itemsets
+        itemSetsByIndex = new ArrayList<int[]>(tempCandidates.values());
+    }
+
+    /** put "true" in trans[i] if the integer i is in line */
+    private static void line2booleanArray(int[] array, boolean[] trans) {
+        Arrays.fill(trans, false);
+        //put the contents of that line into the transaction array
+        for (int i = 0; i < array.length; i++) {
+            int parsedVal = array[i];
+            trans[parsedVal]=true; //if it is not a 0, assign the value to true
+        }
+    }
+
+    private static void calculateFrequentItemsets() throws Exception {
+        List<int[]> frequentCandidates = new ArrayList<int[]>(); //the frequent candidates for the current itemset
+
+        boolean match; //whether the transaction has all the items in an itemset
+        int count[] = new int[itemSetsByIndex.size()]; //the number of successful matches, initialized by zeros
+
+        boolean[] trans = new boolean[integerToStringEncoded.size()];
+
+        // for each transaction
+        int k = 0;
+        while (k < dataEntriesWithEncodedStringIndices.size()) {
+
+            // boolean[] trans = extractEncoding1(data_in.readLine());
+            line2booleanArray(dataEntriesWithEncodedStringIndices.get(k++), trans);
+
+            // check each candidate
+            for (int c = 0; c < itemSetsByIndex.size(); c++) {
+                match = true; // reset match to false
+                // tokenize the candidate so that we know what items need to be
+                // present for a match
+                int[] cand = itemSetsByIndex.get(c);
+                //int[] cand = candidatesOptimized[c];
+                // check each item in the itemset to see if it is present in the
+                // transaction
+                for (int xx : cand) {
+                    if (trans[xx] == false) {
+                        match = false;
+                        break;
+                    }
+                }
+                if (match) { // if at this point it is a match, increase the count
+                    count[c]++;
+                    //log(Arrays.toString(cand)+" is contained in trans "+i+" ("+line+")");
+                }
+            }
+
+        }
+
+        for (int i = 0; i < itemSetsByIndex.size(); i++) {
+            // if the count% is larger than the minSup%, add to the candidate to
+            // the frequent candidates
+            if ((count[i] / (double) (numInstances)) >= minSup) {
+                foundFrequentItemSet(itemSetsByIndex.get(i),count[i]);
+                frequentCandidates.add(itemSetsByIndex.get(i));
+            }
+            //else log("-- Remove candidate: "+ Arrays.toString(candidates.get(i)) + "  is: "+ ((count[i] / (double) numTransactions)));
+        }
+
+        //new candidates are only the frequent candidates
+        itemSetsByIndex = frequentCandidates;
+    }
+
+    /** triggers actions if a frequent item set has been found  */
+    private static void foundFrequentItemSet(int[] itemset, int support) {
+        String New = Arrays.toString(itemset);
+        String New1 = New.substring(0, New.length() - 1) + ", " + support + "]";
+        tupples.add(New1);
+        System.out.println(Arrays.toString(itemset) + "  ("+ ((support / (double) numInstances))+" "+support+")");
+    }
+
+    public static void Lister() {
+        int b = tupples.size();
+        if (b == 0) {
+            System.exit(0);
+        }
+        int i,
+                j,
+                k = 0,
+                m = 0;
+        String newb = tupples.get(b - 1);
+        int a = ((newb.substring(1, newb.length() - 1).split(", ")).length);
+        int[][] lol = new int[b][a - 1];
+        int[] lols = new int[b];
+        for (i = 0; i < b; i++) {
+            newb = tupples.get(i);
+            String[] poop = newb.substring(1, newb.length() - 1).split(", ");
+            for (j = 0; j < poop.length - 1; j++) {
+                lol[i][j] = Integer.parseInt(poop[j]);
+            }
+            lols[i] = Integer.parseInt(poop[j]);
+            if ((j + 1) == a && k == 0) {
+                k = i;
+            }
+            poop = null;
+        }
+        System.out.println("\nAssociation Rules: When Minimum Confidence=" + minConf * 100 + "%");
+        for (i = k; i < b; i++) {
+            for (j = 0; j < k; j++) {
+                m += assoc_print(lol[i], lol[j], lols[i], lols[j]);
+            }
+        }
+        if (m == 0) {
+            System.out.println("No association rules passed the minimum confidence of " + minConf * 100 + "%");
+        }
+    }
+
+    public static int assoc_print(int[] a, int[] b, int a1, int b1) {
+        String win = "(",
+                lose = "(";
+        int i,
+                j,
+                k = 0;
+        int[] loss = new int[a.length];
+        for (i = 0; i < b.length && b[i] != 0; i++) {
+            k = 1;
+            win = win + b[i] + ",";
+            for (j = 0; j < a.length; j++) {
+                if (b[i] == a[j]) {
+                    k = 0;
+                    loss[j] = 1;
+                }
+            }
+        }
+        win = win.substring(0, win.length() - 1) + ")";
+        for (i = 0; i < a.length; i++) {
+            if (loss[i] == 0) {
+                lose = lose + a[i] + ",";
+            }
+        }
+        lose = lose.substring(0, lose.length() - 1) + ")";
+        if (k == 0) {
+            double Lol = (double) a1 / b1;
+            if (Lol > minConf) {
+                System.out.printf("%s ==> %s :	%.2f%c \n", win, lose, Lol * 100, 37);
+                return 1;
+            }
+        }
+        return 0;
     }
 }
